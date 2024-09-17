a8780f2e89383b3e97d27911fc20ef8a5d9760d1
viralijoshi <viralijoshi3110@gmail.com>
2024-09-18 00:03:12 +0530

    Add files via upload

---
+++ task1/script1.js
@@ -0,0 +1,10 @@
+// scripts.js
+window.onscroll = function() {
+    var navbar = document.getElementById("navbar");
+    if (window.scrollY > 50) {
+      navbar.classList.add("scrolled");
+    } else {
+      navbar.classList.remove("scrolled");
+    }
+  };
+  
